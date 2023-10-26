b7bfaa9abcb791ef3d5d3a2d75e45dd3e7288d91
Justin Hui <jhui@lcs.on.ca>
2023-10-26 14:10:45 -0400

    Initial Commit

---
+++ RotatingArrow/RotatingArrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotatingArrowApp.swift
+//  RotatingArrow
+//
+//  Created by Justin Hui on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct RotatingArrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
