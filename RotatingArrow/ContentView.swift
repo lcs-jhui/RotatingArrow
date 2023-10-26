dc8fb4826e7bee2ca5bb7b695a6b63091c49213e
Justin Hui <jhui@lcs.on.ca>
2023-10-26 14:30:14 -0400

    Created an arrow structure and defined the path of the turtle

--- RotatingArrow/ContentView.swift
+++ RotatingArrow/ContentView.swift
@@ -1,21 +1,54 @@
 //
 //  ContentView.swift
-//  RotatingArrow
+//  ArrowExample
 //
-//  Created by Justin Hui on 26/10/2023.
+//  Created by Russell Gordon on 2023-10-26.
 //
-
 import SwiftUI
 
+struct Arrow: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        // Create a path
+        var path = Path()
+        
+        // Define the path
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        
+        //Draw the path
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY / 3 * 2))
+        path.addLine(to: CGPoint(x: rect.maxX / 3 * 2, y: rect.maxY / 3 * 2))
+        path.addLine(to: CGPoint(x: rect.maxX / 3 * 2, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY / 2))
+        path.addLine(to: CGPoint(x: rect.maxX / 3 * 2, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX / 3 * 2, y: rect.maxY / 3))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY / 3))
+                
+        // Return a path
+        return path
+    }
+    
+}
+
 struct ContentView: View {
+    
+    @State var selectedAngle: Double = 0.0
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            Arrow()
+                .aspectRatio(1.0, contentMode: .fit)
+                .rotationEffect(.degrees(selectedAngle))
+            
+            Spacer()
+            
+            Slider(value: $selectedAngle, in: 0...360)
         }
         .padding()
+        
     }
 }
 
